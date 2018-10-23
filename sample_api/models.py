9bde84482d20b2077d727bf72b737f7e2865b90c
Tuấn Vương <vanhtuan0409@gmail.com>
2018-10-23 16:20:36 +0700

    add api

--- sample_api/models.py
+++ sample_api/models.py
@@ -8,3 +8,6 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=255)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
--- sample_api/views.py
+++ sample_api/views.py
@@ -1,6 +1,12 @@
 from django.http import JsonResponse
+import time
+from models import Question
 
 def index(request):
+    for _ in range(3):
+        results = Question.objects.all().values()
+        results = list(results)
+        time.sleep(0.1)
     return JsonResponse({
-        "ok": True
-    })
+        "questions": results
+    }, safe=False)
