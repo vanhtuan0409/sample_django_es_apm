b4bf51cd5f7b51eb5b3217fc4f80163ef5197867
Tuấn Vương <vanhtuan0409@gmail.com>
2018-10-24 11:11:53 +0700

    add custom span

--- sample_api/views.py
+++ sample_api/views.py
@@ -1,12 +1,17 @@
 from django.http import JsonResponse
 import time
 from models import Question
+import elasticapm
+
+@elasticapm.capture_span('send_http_request')
+def send_http_request(index):
+    time.sleep(0.05)
 
 def index(request):
     for _ in range(3):
         results = Question.objects.all().values()
         results = list(results)
-        time.sleep(0.1)
+        send_http_request(_)
     return JsonResponse({
         "questions": results
     }, safe=False)
